f19390e635dcae4a9ece5b4a0ab79cba2e714c55
samuelandazola <szola3@icloud.com>
2018-11-30 11:33:37 -0700

    added GoogleTokenServices class

--- src/main/java/edu/cnm/deepdive/dicewareservice/DicewareServiceApplication.java
+++ src/main/java/edu/cnm/deepdive/dicewareservice/DicewareServiceApplication.java
@@ -1,12 +1,35 @@
 package edu.cnm.deepdive.dicewareservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
+@EnableWebSecurity
+@EnableResourceServer
 @SpringBootApplication
-public class DicewareServiceApplication {
+public class DicewareServiceApplication extends ResourceServerConfigurerAdapter {
+
+	@Value("${oauth.clientId}")
+	private String clientId;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DicewareServiceApplication.class, args);
+	}
+
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.resourceId(clientId);
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeRequests().anyRequest().hasRole("USER");
 	}
 }
